dbbbd730a67237f1c1aa3f00f00d12c83bb6a505
HuoPeng <59678726+GuangDai@users.noreply.github.com>
2023-02-07 15:37:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const express = require('express');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-
-const config = require('./config');
-const routes = require('./routes');
-
-const app = express();
-
-app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: true }));
-app.use(morgan('tiny'));
-
-app.use('/', routes);
-
-app.listen(config.server.port, () => {
-    console.log(`🚀Mercury Parser API listens on port ${config.server.port}`);
-});
-
+const morgan = require('morgan');                                                                                                                                                   
+const cluster = require('cluster')                                                                                                                                                  
+const config = require('./config');                                                                                                                                                 
+const routes = require('./routes');                                                                                                                                                 
+const { Worker } = require("worker_threads");                                                                                                                                       
+const app = express();                                                                                                                                                              
+app.use(helmet());                                                                                                                                                                  
+app.use(bodyParser.urlencoded({ extended: true }));                                                                                                                                 
+app.use(bodyParser.json({ extended: true }));                                                                                                                                       
+app.use(morgan('tiny'));                                                                                                                                                            
+                                                                                                                                                                                    
+app.use('/', routes);                                                                                                                                                               
+if (cluster.isMaster) {                                                                                                                                                             
+    console.log(`Master ${process.pid} is running`)                                                                                                                                 
+    // ...... worker ......                                                                                                                                                         
+    for (let i = 0; i < process.env.FORK_NUMBER; i++) {                                                                                                                             
+        cluster.fork()                                                                                                                                                              
+    }                                                                                                                                                                               
+    cluster.on('exit', (worker, code, signal) => {                                                                                                                                  
+        console.log(`worker ${worker.process.pid} died`)                                                                                                                            
+    })                                                                                                                                                                              
+} else {                                                                                                                                                                            
+    app.listen(config.server.port,()=> {})                                                                                                                                          
+    console.log(`Worker ${process.pid} started`)                                                                                                                                    
+}                                                                                                                                                                                   
 module.exports = app;
