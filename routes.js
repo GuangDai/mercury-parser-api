ef77c31cb5dbac55f5743b75f8329ebab972a1c4
HuoPeng <59678726+GuangDai@users.noreply.github.com>
2023-02-07 15:39:16 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router;
 const router = new Router();
 const Mercury = require('@postlight/mercury-parser');
-
+const {convert} = require('html-to-text');
 router.route('/').get((req, res) => {
     res.json({
-        message: 'Welcome to 🚀mercury-parser-api API! Endpoint: /parser',
+        message: 'Welcome to ....mercury-parser-api API! Endpoint: /parser',
     });
 });
 
@@ -12,7 +12,7 @@ router.route('/parser').get(async (req, res) => {
     let result = { message: 'No URL was provided' };
     if (req.query.url) {
         try {
-            const contentType = req.query.contentType || 'html';
+            const contentType = req.query.contentType || 'markdown';
             let headers = new Object();
             if (typeof req.query.headers !== 'undefined') {
                 headers = JSON.parse(req.query.headers);
@@ -25,6 +25,8 @@ router.route('/parser').get(async (req, res) => {
             result = { error: true, messages: error.message };
         }
     }
+
+}}catch(e){console.log(e.message);}
     return res.json(result);
 });
 
