790f917948fe36e8932bb269be8a37d0be1bddb9
HuoPeng <59678726+GuangDai@users.noreply.github.com>
2023-02-11 17:09:48 +0800

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,6 +3,25 @@ const router = new Router();
 const Mercury = require('@postlight/mercury-parser');
 const {spawn} = require('child_process');
 
+function request_url (url){
+    let dataToSend;
+    console.log(url);
+    let p = new Promise(function (resolve){
+        const python = spawn('python3', ['request_url.py',url]);
+        python.stdout.on('data', function (data) {
+            dataToSend = data.toString();
+            resolve(dataToSend)
+            console.log(dataToSend);
+        });
+        python.stderr.on('data',data=>{console.error(`stderr:${data}`);});
+        python.on('close', (code) => {
+            console.log("Done");
+        });
+    })
+
+    return p;
+}
+
 router.route('/').get((req, res) => {
     res.json({
         message: 'Welcome to ....mercury-parser-api API! Endpoint: /parser',
@@ -11,34 +30,29 @@ router.route('/').get((req, res) => {
 
 router.route('/parser').get(async (req, res) => {
     let result = { message: 'No URL was provided' };
+    let dataToSend;
     if (req.query.url) {
-        if (req.query.url.match(/(\.)google/)){
-            const python = spawn('python3', ['request_url.py',req.query.url]);
-             python.stdout.on('data', function (data) {
-                var dataToSend = data.toString();
-                console.log(dataToSend);
-                req.query.url = dataToSend;
-
-             });
-             python.stderr.on('data',data=>{console.error(`stderr:${data}`);});
-             await python.on('close', (code) => {
-                 console.log("Done");
-             });
-             try {
+        if (req.query.url.match(/(\.)google/)) {
+            try {
                 const contentType = req.query.contentType || 'html';
                 let headers = new Object();
                 if (typeof req.query.headers !== 'undefined') {
                     headers = JSON.parse(req.query.headers);
                 }
-                result = await Mercury.parse(dataToSend, {
-                    contentType,
-                    headers,
-                });
+                result = await request_url(req.query.url).then(
+                    function (data){
+                        let results = Mercury.parse(data, {
+                            contentType,
+                            headers,
+                        });
+                        return results;
+                    }
+                );
+
             } catch (error) {
-                result = { error: true, messages: error.message };
+                result = {error: true, messages: error.message};
             }
-        }
-        else{
+        } else {
             try {
                 const contentType = req.query.contentType || 'html';
                 let headers = new Object();
@@ -50,9 +64,8 @@ router.route('/parser').get(async (req, res) => {
                     headers,
                 });
             } catch (error) {
-                result = { error: true, messages: error.message };
-        }
-        
+                result = {error: true, messages: error.message};
+            }
         }
 
     }
