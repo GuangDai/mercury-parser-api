d313dd27cde7d5b5b69bd2693d56d75965ca5fd1
HuoPeng <59678726+GuangDai@users.noreply.github.com>
2023-02-11 13:30:19 +0800

    Update request_url.py

--- request_url.py
+++ request_url.py
@@ -1,6 +1,14 @@
 import requests
 import sys
+import time
 url = sys[1]
-temp = requests.get(sys[1])
+times = 10
+while times > 0:
+  try:
+    temp = requests.get(sys[1])
+    break
+  except:
+    times -= 10
+    time.sleep(10-times)
 print(temp.url)
 sys.stdout.flush()
